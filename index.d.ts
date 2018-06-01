04a1a2e225008813f4222bca4ea67788c75264b9
sineslu <sineslu@webank.com>
2018-06-01 20:28:53 +0800

    Add 2.1.1

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+export interface GesturePasswordProps {
+    lineWidth?: number,
+    pointBackgroundColor?: string,
+    gestureAreaLength?: number,
+    color?: string,
+    lineColor?: string,
+    activeColor?: string,
+    warningColor?: string,
+    warningDuration?: number,
+    topComponent?: React.ReactElement<any>,
+    bottomComponent?: React.ReactElement<any>,
+    isWarning?: boolean,
+    showArrow?: boolean,
+    allowCross?: boolean,
+    onStart?: () => void,
+    onReset?: () => void,
+    onFinish?: (password: string) => void,
+}
+
+declare class GesturePassword extends React.Component<GesturePasswordProps> { }
+export default GesturePassword;
